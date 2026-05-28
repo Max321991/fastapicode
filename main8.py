1b7cc7cf46bd2377949bf2bbc138177aa410d3de
Manish Gautam <gautammax32@gmail.com>
2026-05-28 10:07:34 +0530

    eigth final commit

--- main8.py
+++ main8.py
@@ -74,3 +74,18 @@ def update_todo(todo_id:int, title:str, db:Session = Depends(get_db)):
         "message":"Todo be update",
         "data": todo
     }
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id:int, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+
+    if not todo:
+        raise HTTPException(status_code=404, detail='Todo not found')
+    
+    db.delete(todo)
+    db.commit()
+    
+    return{
+        "message":"Todo have been Delete",
+        "data": todo
+    }
